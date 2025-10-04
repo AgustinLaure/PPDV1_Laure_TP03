c782d937d9f4a4f43cc483df80a053cc7d061c62
LordMungi <lrdmungi@gmail.com>
2025-10-04 01:14:10 -0300

    added position correction on collision

--- src/Math/form.lua
+++ src/Math/form.lua
@@ -21,23 +21,30 @@ function Form.isColliding (rect1, rect2)
         or rect1.pos.x + rect1.width < rect2.pos.x
         or rect1.pos.y > rect2.pos.y + rect2.height
         or rect1.pos.y + rect1.height < rect2.pos.y) then
+		
         isColliding = false
     end
-
+	
+	if isColliding then
+		distRight = math.abs(rect1.pos.x + rect1.width - rect2.pos.x)
+		distLeft =  math.abs(rect1.pos.x - rect2.pos.x + rect2.width)
+		distDown =  math.abs(rect1.pos.y + rect1.height - rect2.pos.y)
+		distUp =    math.abs(rect1.pos.y - rect2.pos.y + rect2.height)
+		
+		minDist = math.min(distRight, distLeft, distDown, distUp)
+		
+		if minDist == distRight then
+			rect1.pos.x = rect2.pos.x - rect1.width
+		elseif minDist == distLeft then
+			rect1.pos.x = rect2.pos.x + rect2.width
+		elseif minDist == distDown then
+			rect1.pos.y = rect2.pos.y - rect1.height
+		elseif minDist == distUp then
+			rect1.pos.y = rect2.pos.y + rect2.height
+		end
+	end
+	
     return isColliding
-end
-
-function Form.fixPosition(rect1, rect2)
-
-    Form.getHowMuchEntered(rect1, rect2)
-end
-
-function Form.getHowMuchEntered(rect1, rect2)
-    howMuchEntered = {}
-    howMuchEntered.x = 0
-    howMuchEntered.y = 0
-
-    return howMuchEntered
 end
 
 function Form.draw(form)
